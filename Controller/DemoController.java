9011d5c3809d9560c0ba6b795803534dd1d2782d
unknown <sai@gmail.com>
2024-09-02 10:56:58 +0530

    initial project with welcome endpoint

---
+++ Controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.example.demo.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo/api")
+public class DemoController {
+      @GetMapping("/welcome") 
+      public String welcome() {
+    	  return "Welcome to demo application";
+      }
+      
+}
